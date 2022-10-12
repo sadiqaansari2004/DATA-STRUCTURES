bb8877be34dc9131641ab98fc85a0fd38d7b1802
sadiqaansari2004 <114011941+sadiqaansari2004@users.noreply.github.com>
2022-10-13 01:43:39 +0530

    Create 02characterarraypointer.cpp

---
+++ Arrays/02characterarraypointer.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char a[]="sadiqa";
+    char *pa= a;
+    cout<< *pa<<endl;// value at pa
+    cout<< pa<<endl;// array
+    return 0;
+}
