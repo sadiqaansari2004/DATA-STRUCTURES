718fffafe8ada3b8048ef1eb6fc11183e66f0ddc
sadiqaansari2004 <114011941+sadiqaansari2004@users.noreply.github.com>
2023-05-08 20:29:35 +0530

    Create 22 replace pi to pi value.cpp

---
+++ Recursion/22 replace pi to pi value.cpp
@@ -0,0 +1,34 @@
+// program to replace pi with 3.14
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void replace(char *s, int len)
+{
+    if (len == 0 || len == 1)
+        return;
+    if (s[0] == 'p')
+    {
+        if (s[1] == 'i')
+        {
+            for (int i = len; i >= 2; i--)
+                s[i + 2] = s[i];
+            s[0] = '3';
+            s[1] = '.';
+            s[2] = '1';
+            s[3] = '4';
+            replace(s + 4, len - 2);
+        }
+        replace(s + 1, len - 1);
+    }
+    replace(s + 1, len - 1);
+}
+
+int main()
+{
+    char st[50];
+    cout << "input string:" << endl;
+    cin >> st;
+    replace(st, strlen(st));
+    cout << st << endl;
+}
